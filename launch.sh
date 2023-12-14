55863abce2c79ce79f606488218732236980d5e1
luis <lpzr97@gmail.com>
2023-12-14 13:33:30 -0600

    Adding template and condition to pull image

--- launch.sh
+++ launch.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-IMAGE_NAME=docker.io/postgres:14-alpine
+IMAGE_NAME=docker.io/postgres:14.10-alpine
 
-NETWORK_NAME='quini'
+NETWORK_NAME='template'
+
+USER='postgres'
+PASSWORD='postgres'
 
 docker network exists "${NETWORK_NAME}" || docker network create "${NETWORK_NAME}"
+
+# pull image if it does not exists
+if ! docker image inspect "$IMAGE_NAME" &> /dev/null; then
+    docker pull "$IMAGE_NAME"
+fi
 
 # folder where to store the postgresql data
 DATA_FOLDER="$(pwd)/$(dirname "$0")/data/"
@@ -23,8 +31,8 @@ docker run \
     --rm \
     --name "${CONTAINER_NAME}" \
     --publish 127.0.0.1:5432:5432 \
-    --env POSTGRES_USER=postgres \
-    --env POSTGRES_PASSWORD=postgres \
+    --env POSTGRES_USER="${USER}" \
+    --env POSTGRES_PASSWORD="${PASSWORD}" \
     --volume "${DATA_FOLDER}":/var/lib/postgresql/data \
     --network "${NETWORK_NAME}" \
     "${IMAGE_NAME}"
